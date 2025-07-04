9102d5ab71f2cb0d119e763f5d954b1e68ebe8d8
Blazhe Manev <blaze_manev@yahoo.com>
2025-07-04 11:17:48 +0200

    * install and configure PrimeNG with theme and icons * implement overview table with PrimeNG and basic student actions

--- src/app/features/overview/overview.component.ts
+++ src/app/features/overview/overview.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { Student } from '../../core/models/student.model';
+import { StudentService } from '../../core/services/student.service';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TableModule, ButtonModule],
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.scss'
+  styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+  students: Student[] = [];
 
+  constructor(private studentService: StudentService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents(): void {
+    this.studentService.getAll().subscribe((data) => {
+      this.students = data;
+    });
+  }
+
+  onAdd(): void {
+    this.router.navigate(['/student/add']);
+  }
+
+  onEdit(id: number): void {
+    this.router.navigate(['/student/edit', id]);
+  }
+
+  onDelete(id: number): void {
+    this.studentService.delete(id).subscribe(() => {
+      this.loadStudents(); // reload after delete
+    });
+  }
 }
