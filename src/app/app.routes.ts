295559b876b6904372fe82b35a6c64117dfd9664
Blazhe Manev <blaze_manev@yahoo.com>
2025-07-04 11:01:59 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full'
+  },
+  {
+    path: 'overview',
+    loadChildren: () =>
+      import('./features/overview/overview.routes').then((m) => m.OVERVIEW_ROUTES),
+  },
+  {
+    path: 'student',
+    loadChildren: () =>
+      import('./features/student/student.routes').then((m) => m.STUDENT_ROUTES),
+  }
+];
