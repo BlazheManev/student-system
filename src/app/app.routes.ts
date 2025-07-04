8fe96c4dff0402743ebeb2ce2e93bcafdcf4f100
Blazhe Manev <blaze_manev@yahoo.com>
2025-07-04 11:10:25 +0200

    *  add student route definition with StudentFormComponent * add overview route definition with OverviewComponent * flatten component folder structure for clarity *  correct relative paths for lazy-loaded route import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'overview',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'overview',
     loadChildren: () =>
-      import('./features/overview/overview.routes').then((m) => m.OVERVIEW_ROUTES),
+      import('../app/features/overview/overview.routes').then((m) => m.OVERVIEW_ROUTES),
   },
   {
     path: 'student',
     loadChildren: () =>
-      import('./features/student/student.routes').then((m) => m.STUDENT_ROUTES),
+      import('../app/features/student/student.routes').then((m) => m.STUDENT_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: 'overview',
   }
 ];
---
+++ src/app/features/student/student.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { StudentFormComponent } from './student-form.component';
+
+export const STUDENT_ROUTES: Routes = [
+  {
+    path: 'add',
+    component: StudentFormComponent,
+  },
+  {
+    path: 'edit/:id',
+    component: StudentFormComponent,
+  }
+];
