4380a8938e3508d8eb208055305a284e8c8a53b8
Blazhe Manev <blaze_manev@yahoo.com>
2025-07-04 19:57:26 +0200

    change to prod

--- src/app/core/services/student.service.ts
+++ src/app/core/services/student.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Student } from '../models/student.model';
-import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.prod';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class StudentService {
@@ -14,19 +14,21 @@ export class StudentService {
     return this.http.get<Student[]>(this.apiUrl);
   }
 
-  getById(id: number): Observable<Student> {
-    return this.http.get<Student>(`${this.apiUrl}/${id}`);
+  getById(id: number): Observable<Student | undefined> {
+    return this.http.get<Student[]>(this.apiUrl).pipe(
+      map(students => students.find(student => student.id === id))
+    );
   }
 
   create(student: Student): Observable<Student> {
-    return this.http.post<Student>(this.apiUrl, student);
+    throw new Error('Create not supported with static JSON');
   }
 
   update(id: number, student: Student): Observable<Student> {
-    return this.http.put<Student>(`${this.apiUrl}/${id}`, student);
+    throw new Error('Update not supported with static JSON');
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    throw new Error('Delete not supported with static JSON');
   }
 }
